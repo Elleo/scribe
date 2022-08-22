bdbbbd2db6f7bd6e27fb54c9f5966c1131f4d688
Mike Sheldon <mike@mikeasoft.com>
2022-08-22 18:27:47 +0100

    Use gstreamer for audio input

--- scriberecognition/scriberecognition.py
+++ scriberecognition/scriberecognition.py
@@ -1,33 +1,61 @@
 #!/usr/bin/env python3
 
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst, GLib, GObject
 from vosk import Model, KaldiRecognizer
 import sys
 import os
 import wave
 import re
-import sounddevice as sd
 import queue
+import json
 
-q = queue.Queue()
-device_info = sd.query_devices(0, 'input')
 
-def callback(indata, frames, time, status):
-    if status:
-        q.put(bytes(indata))
+class AudioProcessor(object):
 
-tf = open(sys.argv[2], "r")
-text = tf.read()
+    def __init__(self):
+        self.pipeline = Gst.parse_launch("autoaudiosrc ! audio/x-raw,format=S16LE,channels=1,rate=16000 ! audioconvert ! audiorate ! appsink name=sink emit-signals=True")
+        sink = self.pipeline.get_by_name("sink")
+        sink.connect("new-sample", self.on_new_sample)
+        self.load_model()
 
-words = str(re.split("[.,;!?\-\n]", text.lower())).replace(" '", " \"").replace("',", "\",").replace("['", "[\"").replace("']", "\"]")
-
-model = Model(lang="en-us")
-
-rec = KaldiRecognizer(model, 16000, words)
-
-with sd.RawInputStream(samplerate=16000, blocksize = 8000, device=1, dtype='int16', channels=1, callback=callback):
-    while True:
-        data = q.get()
-        if rec.AcceptWaveform(data):
-            print(rec.Result())
+    def load_model(self, text="", language="en-us"):
+        words = str(re.split("[.,;!?\-\n]", text.lower())).replace(" '", " \"").replace("',", "\",").replace("['", "[\"").replace("']", "\"]")
+        model = Model(lang=language)
+        if words != '[""]':
+            self.rec = KaldiRecognizer(model, 16000, words)
         else:
-            print(rec.PartialResult())
+            print("Unconstrained")
+            self.rec = KaldiRecognizer(model, 16000)
+
+    def start(self):
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+    def pause(self):
+        self.pipeline.set_state(Gst.State.PAUSED)
+
+    def stop(self):
+        self.pipeline.set_state(Gst.State.STOPPED)
+
+    def on_new_sample(self, sink):
+        sample = sink.emit("pull-sample")
+        buf = sample.get_buffer()
+        success, map_info = buf.map(Gst.MapFlags.READ)
+        final = False
+        if self.rec.AcceptWaveform(map_info.data):
+            text = json.loads(self.rec.Result())['text']
+            final = True
+        else:
+            text = json.loads(self.rec.PartialResult())['partial']
+        if len(text) > 0:
+            print(text)
+        return Gst.FlowReturn.OK
+
+
+if __name__ == "__main__":
+    Gst.init(None)
+    ap = AudioProcessor()
+    ap.start()
+    loop = GLib.MainLoop()
+    loop.run()
