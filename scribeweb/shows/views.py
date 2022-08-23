e898dccb6a829d658b36a2d5efe375e0439e9314
Mike Sheldon <mike@mikeasoft.com>
2022-08-23 20:28:16 +0100

    Rename templates

--- scribeweb/shows/views.py
+++ scribeweb/shows/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('shows.html')
     context = {}
     return HttpResponse(template.render(context, request))
