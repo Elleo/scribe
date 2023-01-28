77c1ac2a514ffa1fe60af37efeccc4888383cc51
Mike Sheldon <mike@mikeasoft.com>
2023-01-28 19:11:45 +0000

    Handle multiple file formats for scripts via pandoc

--- scribeweb/shows/views.py
+++ scribeweb/shows/views.py
@@ -1,3 +1,4 @@
+import pandoc
 from django.forms.models import model_to_dict
 from django.shortcuts import render, redirect
 from django.template import loader
@@ -44,7 +45,9 @@ def add_or_edit(request):
         if show_form.is_valid():
             show = show_form.save()
             if 'script_file' in request.FILES:
-                show.script = request.FILES['script_file'].read().decode()
+                doc_format = pandoc.format_from_filename(request.FILES['script_file'].name)
+                pd = pandoc.read(request.FILES['script_file'].read(), format=doc_format)
+                show.script = pandoc.write(pd, format="plain")
                 show.save()
             return redirect("/shows")
         else:
