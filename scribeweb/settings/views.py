c71af7a2e5716a72ae60eafb6ec12a2b3eb8825a
Mike Sheldon <mike@mikeasoft.com>
2023-01-29 14:57:33 +0000

    Add display of available wifi networks and current network

--- scribeweb/settings/views.py
+++ scribeweb/settings/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .wifi import Wifi
 
 # Create your views here.
 def index(request):
     template = loader.get_template('settings.html')
-    context = {}
+    wifi = Wifi()
+    context = {'networks': wifi.networks,
+            'current_network': wifi.current_network}
     return HttpResponse(template.render(context, request))
