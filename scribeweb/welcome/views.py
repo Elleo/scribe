836f07f6025d3cb6fcd02a15c3ae4a6b51c572fb
Mike Sheldon <mike@mikeasoft.com>
2023-01-30 18:54:05 +0000

    Display currently active show

--- scribeweb/welcome/views.py
+++ scribeweb/welcome/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from shows.models import Show
 
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    shows = Show.objects.all()
+    context = {'shows': shows}
     return HttpResponse(template.render(context, request))
